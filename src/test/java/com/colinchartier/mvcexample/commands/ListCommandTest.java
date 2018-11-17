2e4ff7c8a531d8be821180d8fa49969e2d01d3d0
Colin Chartier <colin.n.chartier@gmail.com>
2018-11-17 17:33:41 -0500

    Add mockito.

--- src/test/java/com/colinchartier/mvcexample/commands/ListCommandTest.java
+++ src/test/java/com/colinchartier/mvcexample/commands/ListCommandTest.java
@@ -4,37 +4,27 @@ import com.colinchartier.mvcexample.Filesystem;
 import com.colinchartier.mvcexample.Terminal;
 
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class ListCommandTest {
 
-  //test the ListCommand class
-  private String whatWasPrinted = null;
-
   @Test
-  public void testPrintsOneFile() {
-    ListCommand ls = new ListCommand(
-        new Terminal() {
-          public void printLine(String message) {
-            whatWasPrinted = message;
-          }
+  public void testPrintsOneFile() throws IOException {
+    Terminal terminal = mock(Terminal.class);
+    when(terminal.promptLine()).thenReturn("ls /a");
 
-          public String promptLine() throws IOException {
-            return "ls /a";
-          }
-        }, new Filesystem() {
-          public String[] listDirectoryFiles(String file) {
-            if("/a".equals(file)) {
-              return new String[] {"hello"};
-            }
-            return new String[] {"sad"};
-          }
-        }
-    );
+    Filesystem fs = mock(Filesystem.class);
+    when(fs.listDirectoryFiles("/a")).thenReturn(new String[]{"hello"});
 
-    assert ls.exec(new String[]{"/a"}); //runs it, asserts it is true
-    //if this is not hello, we are not helloing today.
-    assert whatWasPrinted.equals("hello");
+    ListCommand ls = new ListCommand(terminal, fs);
+    assertTrue(ls.exec(new String[]{"/a"}));
+    verify(terminal).printLine(ArgumentMatchers.eq("hello"));
   }
 }
